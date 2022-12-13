ffce7f10d2ced4aec10c26d1b853e1ea58887610
JOAO VITOR PRIMO DE OLIVEIRA <jv.primo26@gmail.com>
2022-12-12 21:31:59 -0300

    ToDo List para teste de react Testing Library feito

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
+import { useState} from 'react';
 import './App.css';
+import styled from "styled-components"
 
 function App() {
+  const [tarefas, setTarefas] = useState([]);
+  const [newTarefa, setNewTarefa] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!newTarefa || tarefas.includes(newTarefa)) return;
+
+    setTarefas([...tarefas, newTarefa]);
+    setNewTarefa("");
+  }
+
+  function handleDelete(taf) {
+    setTarefas(tarefas.filter((tafItem) => tafItem !== taf));
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Text>
+          Lista de tarefas a serem relizadas no dia 12/12/2022
+        </Text>
+       <ul data-testid="ul-tarefas">
+      {tarefas.length !== 0 ?
+      <>
+      {tarefas.map((taf) => (
+        <li data-testid={taf} key={taf}>
+        {taf}
+        <Button
+        data-testid={`${taf}-btn-delete`}
+        type="button"
+        onClick={() =>  handleDelete(taf)}
         >
-          Learn React
-        </a>
+        ❌
+        </Button>
+        </li>
+      ))}
+      </> : 
+      <>
+      <p>
+        Ainda não há tarefas para o dia
+      </p>
+       </>}
+       </ul>
+       <Form data-testid="form-add-tarefa" onSubmit={handleSubmit}>
+        <input
+        data-testid="input-add-tarefa"
+        type="text"
+        value={newTarefa}
+        onChange={(e) => setNewTarefa(e.target.value)}/>
+        <Button type="submit">Salvar</Button>
+       </Form>
       </header>
     </div>
   );
 }
 
+const Text = styled.p`
+`
+const Button = styled.button`
+`
+
+const Form = styled.form`
+`
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,70 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event"
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//importar o fireEvent e o userEvent, o primeiro é utilizado para disparar eventos, como cliques, mudanças de textos e submits.
+//Já o Segundo fornece métodos que aproximam a realidade da iteração do usuário com a tela, como o "type" que dispara eventos como click, keydown, keyUp;
+//É preferível que se use o userEvent ao fireevent
+// A estrutura dos testes com jest e com react testing Library é a mesma;
+describe('Testing App.js', () => {
+  it("It Should be able to show the component Text", ()=>{
+    //render APP é a forma com que mandamos o teste rendenizar o componente, retornando um render Result.
+    render(<App />);
+    //o método  screen é utilizados para fazer consultas  no DOM.
+    const linkElement = screen.getByText(/Lista de tarefas a serem relizadas no dia/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("It Should be able to not render the list of activeties when the array is empty", ()=>{
+    render(<App />);
+    const linkElement = screen.getByText(/Ainda não há tarefas para o dia/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  it("It Should be able to add a new tarefa", ()=>{
+    render(<App />);
+// Buscamos o input de texto usando a query screen.getByTestId e o formulário que possui o data-testid="form-add-tech". É nesse momento que usamos os ids que definimos lá no componente.
+    const input = screen.getByTestId("input-add-tarefa");
+    const form = screen.getByTestId("form-add-tarefa");
+// com o input e o form encontrados, podemos realizar ações em cima deles.
+//o userEvent.type é responsável por preencher o campo input com "codar".
+    userEvent.type(input, "codar");
+// já o fireEvent.submit é responsǘael pela submissão desta informação no formulário.    
+    fireEvent.submit(form);
+//Espero que "codar" exista, ou seja o resultado da query screen.getByTestId("codar") seja true (verdadeiro). Ao contrário, retornaria falso e o teste falharia.
+    expect(screen.getByTestId("codar")).toBeTruthy();
+  });
+
+  it("It Should be able to list 2 tarefas", ()=>{
+    render(<App />);
+
+    const input = screen.getByTestId("input-add-tarefa");
+    const form = screen.getByTestId("form-add-tarefa");
+// neste caso somente mostrando que também é possível realizar o preenchimento de um campo com o fireEvent.
+    fireEvent.change(input,{ target: {value:"codar"} });
+    fireEvent.submit(form);
+
+    fireEvent.change(input,{ target: {value:"ira a academia"}});
+    fireEvent.submit(form);
+
+    const listOfTaferas = screen.getByTestId("ul-tarefas");
+    expect(listOfTaferas.children.length).toBe(2);
+  })
+
+  it("It Should be able to delete 1 tarefa", ()=>{
+    render(<App />);
+
+    const input = screen.getByTestId("input-add-tarefa");
+    const form = screen.getByTestId("form-add-tarefa");
+    userEvent.type(input, "codar");
+    fireEvent.submit(form);
+
+// neste caso somente mostrando que também é possível realizar o submit a partir do click do userEvent.
+    const itemButtonDelete = screen.getByTestId("codar-btn-delete");
+    userEvent.click(itemButtonDelete);
+    
+    expect(screen.queryByTestId("codar")).toBeNull();
+  })
+
 });
+
